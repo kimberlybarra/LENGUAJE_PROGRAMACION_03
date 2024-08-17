6e4ba7e201c309bebc85b938e619917f4a572c14
kimberlybarra <164095918+kimberlybarra@users.noreply.github.com>
2024-08-16 23:07:08 -0500

    prefijo y postfijo

--- sesion1/src/sesion1/hola_mundo.java
+++ sesion1/src/sesion1/hola_mundo.java
@@ -3,7 +3,7 @@ package sesion1;
 public class hola_mundo {
 	
 		public static void main(String[] args) {
-			System.out.println("HOLA MUNDO DESDE JAVA SOY KIMBERLY");
+			System.out.println("HOLA MUNDO DESDE JAVA ESTOY APRENDIENDO");
 		}
 		//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 
 		//la contraseña del repositorio
