18dbb4d9d84c2d358fabc737905f88a458db4a05
kimberlybarra <164095918+kimberlybarra@users.noreply.github.com>
2024-08-17 00:18:58 -0500

    comentarios

--- sesion1/src/sesion1/hola_mundo.java
+++ sesion1/src/sesion1/hola_mundo.java
@@ -1,6 +1,7 @@
 package sesion1;
 
 public class hola_mundo {
+//no volver a dibulgar datos personales
 	
 		public static void main(String[] args) {
 			System.out.println("HOLA MUNDO DESDE JAVA ESTOY APRENDIENDO");
