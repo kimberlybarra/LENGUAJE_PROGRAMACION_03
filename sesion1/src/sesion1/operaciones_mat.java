76a69126838dae100ffe872cc30e2464293bd9d5
kimberlybarra <164095918+kimberlybarra@users.noreply.github.com>
2024-08-17 10:44:23 -0500

    operaciones matematicas

---
+++ sesion1/src/sesion1/operaciones_mat.java
@@ -0,0 +1,14 @@
+package sesion1;
+
+public class operaciones_mat {
+	public static void main(String[] args) {
+		double a = 2.00;
+		double b = 3.00;
+		System.out.println(Math.PI);
+		System.out.println(Math.ceil(a));//redondea el numero decimal al entero mas cercano arriba
+		System.out.println(Math.floor(b));//redondea el num decimal al entero mas cercano hacia abajo
+		System.out.println(Math.pow(a, b));//exponencial a elevado a b
+		System.out.println(Math.max(a, b));//devuelve el numero mayor
+		System.out.println(Math.sqrt(16));//devuelve la raiz cuadrada
+	}
+}
