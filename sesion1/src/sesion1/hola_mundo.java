e2c6b78bfe96a6d037e032785206ea7d193bcc4f
kimberlybarra <164095918+kimberlybarra@users.noreply.github.com>
2024-08-17 00:17:36 -0500

    correjido

--- sesion1/src/sesion1/hola_mundo.java
+++ sesion1/src/sesion1/hola_mundo.java
@@ -4,9 +4,6 @@ public class hola_mundo {
 	
 		public static void main(String[] args) {
 			System.out.println("HOLA MUNDO DESDE JAVA ESTOY APRENDIENDO");
-		}
-		//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 
-		//la contraseña del repositorio
-		
+		}		
 }
 
