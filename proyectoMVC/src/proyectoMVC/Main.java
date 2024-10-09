c2bdd8c845d9a24825704a9326e78ea7e91d016f
kimberlybarra <164095918+kimberlybarra@users.noreply.github.com>
2024-10-09 15:45:27 -0500

    MVC

--- proyectoMVC/src/proyectoMVC/Main.java
+++ proyectoMVC/src/proyectoMVC/Main.java
@@ -1,19 +1,29 @@
 package proyectoMVC;
 
-
 public class Main {
-	public static void main (String[] args) {
-		
-		TaskView view = new TaskView ();
-		TaskController controller = new TaskController(view);
-		
-		controller.addtask(" TERMINAR TRABAJO DE LP");
-		controller.addtask(" TERMINAR INFORME DE REDES");
-		
-		controller.displayTask();
-		
-		controller.completeTask(1);
-		
-		controller.displayTask();
-	}
+    public static void main(String[] args) {
+        
+        TaskView view = new TaskView();
+        TaskController controller = new TaskController(view);
+        
+        controller.addtask("Terminar trabajo de LP");
+        controller.addtask("Terminar informe de redes");
+        
+        controller.displayTask();
+        
+        controller.completeTask(1);
+        
+        controller.displayTask();
+//actualiazr
+        controller.updateTask(0, "Finalizar trabajo de LP");
+        controller.displayTask();
+//eliminar        
+        controller.deleteTask(1);
+        controller.displayTask();
+        
+        controller.saveTasksToFile("tareas.txt");
+
+        controller.loadTasksFromFile("tareas.txt");
+        controller.displayTask();
+    }
 }
