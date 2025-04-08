47415ebf1cdd39184da3c4eff8d10800a2f6a14b
kimberlybarra <164095918+kimberlybarra@users.noreply.github.com>
2025-04-07 22:17:49 -0500

    Add files via upload

---
+++ sesion4/Hanoi.java
@@ -0,0 +1,26 @@
+package sesion4;
+
+public class Hanoi {
+	
+static int contador =1;
+
+	public static void main(String[] args) {
+	torresHanoi(4,1,2,3);
+
+}
+	
+	//Creando el metodo recursivo para la solucion de las torres hanoi
+	public static void torresHanoi(int discos, int torre1, int torre2, int torre3){
+		//Caso base
+		if (discos == 1){
+			System.out.println("movimiento:  "+ contador++ +" -> mover disco de torre " + torre1 + " a torre " + torre3);
+		} else{
+			//Dominio (problema– 1)
+				torresHanoi(discos-1, torre1,torre3, torre2);
+				System.out.println("movimineto: "+ contador++ +" -> mover disco de torre "+ torre1 + " a torre " + torre3);
+				torresHanoi(discos-1, torre2, torre1, torre3);
+		}
+	}
+
+}
+
